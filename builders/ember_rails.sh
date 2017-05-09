d2398826f4527a1a0911530c89a5f6ebd4d8cecd
Jono <jono@foodnotblogs.com>
2017-05-09 06:15:01 -0700

    Trying to get akita to build again without a seperate build step. Using alpine and node, but cant get node-sass to work in alpine.

--- builders/ember_rails.sh
+++ builders/ember_rails.sh
@@ -10,10 +10,3 @@ IMAGE_NAME=$2
 build_image_compiled \
     $PROJECT_DIR $IMAGE_NAME \
     nodesource/trusty:0.12.9 dummy_repo /tmp/dummy
-
-
-
-
-
-
-# docker run --volume $(pwd):/frontend nodesource/trusty:0.12.9 sh -c 'cd /frontend && bin/setup'
