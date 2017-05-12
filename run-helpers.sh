ba1c0450035e3fef31aa2219d7089019ac61ac1a
Jono Finger <jfinger@plos.org>
2017-05-12 11:18:29 -0700

    Fixing run helper for starting tomcat on another port

--- run-helpers.sh
+++ run-helpers.sh
@@ -25,14 +25,16 @@ function require_mysql_envs {
 }
 
 
-# TODO: depricate in favor of start_tomcat.sh
+# TODO: depricate in favor of start_tomcat.sh, though solr might depend on it?
 function start_tomcat {
 
   # hack to prevent port conflicts when using host networking mode
 
-  [[ -n $TOMCAT_HTTP_PORT ]] && sed -i -e 's/8080/$TOMCAT_HTTP_PORT/g' $CATALINA_HOME/conf/server.xml
+  [[ -n $TOMCAT_HTTP_PORT ]] && sed -i -e "s/8080/$TOMCAT_HTTP_PORT/g" $CATALINA_HOME/conf/server.xml
 
-  [[ -n $TOMCAT_CONTROL_PORT ]] && sed -i -e 's/8005/$TOMCAT_CONTROL_PORT/g' $CATALINA_HOME/conf/server.xml
+  [[ -n $TOMCAT_CONTROL_PORT ]] && sed -i -e "s/8005/$TOMCAT_CONTROL_PORT/g" $CATALINA_HOME/conf/server.xml
+
+  # end hack. note this might break the HEALTHCHECK
 
 	${CATALINA_HOME}/bin/catalina.sh run
 }
